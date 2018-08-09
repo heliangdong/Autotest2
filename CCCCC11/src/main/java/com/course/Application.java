9f22b15723a2c2c5f2527da7f9e7149bb95e208d
heliangdong <wb-hld409227@alibaba-inc.com>
2018-08-09 18:14:34 +0800

    20180809

--- CCCCC11/src/main/java/com/course/Application.java
+++ CCCCC11/src/main/java/com/course/Application.java
@@ -1,4 +1,6 @@
-package com.course.model;
+package com.course;
+
+
 
 
 import org.springframework.boot.SpringApplication;
@@ -13,11 +15,14 @@ import javax.annotation.PreDestroy;
 public class Application {
 
     private  static ConfigurableApplicationContext context;
+
     public static void main(String[] args) {
-        Application.context= SpringApplication.run(Application.class,args);
+        Application.context = SpringApplication.run(Application.class,args);
     }
+
     @PreDestroy
     public void close(){
         Application.context.close();
     }
+
 }
